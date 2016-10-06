5023298451387479d82853feb992fd4c30b1e657
rogerlevy <roger.levy@gmail.com>
2016-10-06 18:53:46 -0400

    progress

--- stranger/stranger.f
+++ stranger/stranger.f
@@ -1,11 +1,12 @@
 
-\ requires bubble/core/core.f
-\ doesn't work according to forth2012?
+\ require bubble/core/core.f
+\ `require` doesn't work in swiftforth
 
 idiom [stranger]
-
-
 cd stranger
+
+\ -------------------------------------------------------------------------------------------------
+
 z" Sprite-0001.png" al_load_bitmap
 
 : celltbl  create does> swap cells + @ ; \ todo: to change the table, we'll provide a special word...
@@ -15,16 +16,18 @@ dup , dup , dup , dup ,
 dup , dup , dup , dup ,
 dup , dup , dup ,     ,
 
+\ -------------------------------------------------------------------------------------------------
 
 : cls  clear-to-color ;
 
+\ -------------------------------------------------------------------------------------------------
 
 decimal
 create sizes
-16 , 16 ,   16 , 32 ,   32 , 32 ,   16 , 16 ,
-16 , 16 ,   16 , 32 ,   32 , 32 ,   16 , 16 ,
-16 , 16 ,   16 , 32 ,   32 , 32 ,   16 , 16 ,
-16 , 16 ,   16 , 32 ,   32 , 32 ,   16 , 16 ,
+16 , 16 ,   32 , 32 ,    64 , 64 ,    128 , 128 ,
+16 , 32 ,   16 , 64 ,    32 , 64 ,    64 , 128 ,
+32 , 16 ,   64 , 16 ,    64 , 32 ,    128 , 64 ,
+48 , 48 ,   128 , 256 ,  256 , 128 ,  256 , 256 ,
 
 : @size  2 cells * sizes + 2v@ ;
 
@@ -43,6 +46,7 @@ create sizes
 ;
 fixed
 
+\ -------------------------------------------------------------------------------------------------
 : var  create dup , cell+ immediate does> @ " me ?lit + " evaluate ;             ( total -- <name> total+cell )
 
 \ - con = constant data
@@ -50,39 +54,100 @@ fixed
 \ - 5 cells left over
 0 var x var y var vx var vy var con var flags var 'act var 'draw
   var rs 7 cells + var ds 7 cells +
-  var rsp var sp var zorder
-constant /ent
+  var rp var sp var zorder
+value /ent
 
 
 \ flags
 #1
     bit en#
+    bit vis#
     bit persist#
     bit unload#
     \ current animation frame (4 bits)
     \ current animation counter (4 bits)
 drop
 
+\ -------------------------------------------------------------------------------------------------
+
 32 cells constant /slot
 4096 constant #maxents
 create pool  #maxents /slot * /allot
 0 value whom  \ ID of me
 
-: adr  dup #maxents >= abort" Illegal entity" /slot * pool + ;
+: adr  dup 0 < over #maxents >= or abort" Illegal entity" /slot * pool + ;
 : >id  pool - /slot / ;
-: as  dup to whom  adr to me ;
-: {   me >r  as ;
-: }   r> dup to me  >id to whom ;
+: as  dup to whom  adr to me ; 0 as
+\ : {   me >r  as ;
+\ : }   r> dup to me  >id to whom ;
 : me  me  >id ;
-
-0 as
-
+: nxt  whom 1 + 4095 and as ;
 : flag?  flags @ and 0<> ;
 : en?  en# flag? ;
+: set  flags or! ;
+: unset  flags not! ;
+: draw  r> 'draw ! ;
+: act   r> 'act ! ;
+: init  en# vis# or flags !  0 0 vx 2v!  at@ x 2v!  act draw noop ;
+: new  4096 0 do  en? 0= if init unloop exit then  nxt loop  -1 abort" Out of entities" ;
+: single  new me constant  persist# set ;
+
+\ -------------------------------------------------------------------------------------------------
+
+single main
+
+: perform ( n -- <code> )
+    ds 7 cells + !
+    ds 6 cells + sp !
+    r> rs 7 cells + !
+    rs 7 cells + rp !
+;
+
+\ important: this word must not CALL anything or use the return stack until the bottom part.
+: yield
+    \ save state
+    dup \ ensure TOS is on stack
+    sp@ sp !
+    rp@ rp !
+    \ look for next task/actor.  rp=0 means no task.  end of list = jump to main task and resume that
+    begin  nxt  me main = if  rp @  else  main to me  true  then  until
+    \ restore state
+    rp @ rp!
+    sp @ sp!
+    drop \ ensure TOS is in TOS register
+;
+
+: end  0 rp! yield ;
+: frames  0 do yield loop ;
+: secs  60 * frames ;
+: multi
+    me >r
+    dup
+    stage first @ main next !
+    sp@ main 's sp !
+    rp@ main 's rp !
+    main to me
+    ['] yield catch
+    ?dup if
+        main to me
+        rp @ rp!
+        sp @ sp!
+        drop
+        throw
+    then
+    drop
+    r> as
+;
+\ : :proc  actor single :noname 'act ! ;
+\ : :task  actor single :noname 0 me perform ;
+
+\ -------------------------------------------------------------------------------------------------
 
 
-: ent  ;
 
+
+
+\ -------------------------------------------------------------------------------------------------
 
 : ok  go  step  show  0 0 0 cls ;
 ok
